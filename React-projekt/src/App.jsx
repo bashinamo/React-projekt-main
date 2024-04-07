471c6f9022749d630d570c78e49c61beec90c9a0
bashin <goblinsgoldg@gmail.com>
2024-04-07 21:02:22 +0200

    Individuellt React-projekt

---
+++ React-projekt/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import './Header.Section.css'
+import Section from './Section'
+
+
+function App(){
+
+return (
+  <>
+    {/* <Header /> */}
+    <Section />
+ 
+  </>
+)
+
+}
+export default App;
+
+
